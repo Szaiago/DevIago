334a2307cd31c326eab7a0e1d70e8582aeb94319
Iago Souza <iago.snd.souza@gmail.com>
2024-07-12 00:55:17 -0300

    atualizações

--- js/animation-monitor.js
+++ js/animation-monitor.js
@@ -1,12 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('start').addEventListener('click', function() {
+    var startButton = document.getElementById('start');
+    var isShutdown = false;
+
+    startButton.addEventListener('click', function() {
         var telaMonitor = document.querySelector('.tela-monitor');
+
+        // Altera o estado do botão
+        isShutdown = !isShutdown;
+        if (isShutdown) {
+            startButton.textContent = 'SHUTDOWN';
+            startButton.classList.add('shutdown');
+        } else {
+            startButton.textContent = 'START';
+            startButton.classList.remove('shutdown');
+        }
+
+        // Restante do seu código aqui...
+
+        // Função para rolar para o final
+        function scrollToBottom() {
+            telaMonitor.scrollTop = telaMonitor.scrollHeight;
+        }
 
         // Comandos a serem exibidos
         const comandos = [
             "@echo off",
             "setlocal",
             "",
+            "rem Define o caminho de cada diretório",
             'set "base_path=C:\\Arquivos"',
             'set "path1=%base_path%\\Este Computador"',
             'set "path2=%path1%\\Usuarios"',
@@ -15,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
             'set "path5=%path4%\\Arquivos-Usuario01"',
             'set "final_path=%path5%\\Portfolio DevIago"',
             "",
+            "rem Navegar passo a passo",
             "echo Navegando para %base_path%",
             'cd /d "%base_path%"',
             "",
@@ -36,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
             "echo Navegando para %final_path%",
             'cd "Portfolio DevIago"',
             "",
+            "echo Navegação concluída. Você está agora em %cd%"
         ];
 
         // Mostra tela de carregamento
@@ -54,6 +77,7 @@ document.addEventListener('DOMContentLoaded', function() {
             comandos.forEach((comando, index) => {
                 setTimeout(() => {
                     typingAnimation.innerHTML += `<div class="typing-text">${comando}</div><br>`;
+                    scrollToBottom(); // Rola para o final a cada novo comando
                 }, index * 1000); // Ajuste o tempo de exibição de cada linha aqui
             });
         }, 5000); // Após 5 segundos troca para a animação de digitação
